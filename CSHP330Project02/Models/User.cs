c3077a28cdae20990d36d33ba420b92d8af37149
Glauber Vilane Ferreira Marques <gvfmarques@hotmail.com>
2021-08-31 21:33:57 -0500

    First commit, launching application

---
+++ CSHP330Project02/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSHP330Project02.Models
+{
+     public class User
+     {
+         [JsonProperty("Id")]
+         public Guid Id { get; set; }
+
+         [JsonProperty("Email")]
+         public string UserEmail { get; set; }
+
+         [JsonProperty("Password")]
+         public string UserPassword { get; set; }
+
+         [JsonProperty("DateCreated")]
+         public DateTime DateCreated { get; set; }
+     }
+}
